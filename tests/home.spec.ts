2fb60240666c8197d957f323dd5a0948d42dbe8a
AnnaHomeTest <anna.swierkosz81@gmail.com>
2025-07-12 18:17:31 +0100

    test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,7 +4,6 @@ test("Home page", async ({ page }) => {
   await page.goto("https://practicesoftwaretesting.com/");
   // Ensure the sign-in link is present
   await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
-<<<<<<< Updated upstream
 
   // Check the title of the page
   await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
@@ -15,15 +14,6 @@ test("Home page", async ({ page }) => {
   expect(await productGrid.getByRole("link").count()).toBe(9);
 
   // Search for Thor Hammer and check the result
-=======
-  //Check the title of the page
-  await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
-  //Check the count of items displayed
-  const productGrid = page.locator(".col-md-9");
-  await expect(productGrid.getByRole("link")).toHaveCount(9);
-  expect(await productGrid.getByRole("link").count()).toBe(9);
-  //Search for Thor Hammer and check the results
->>>>>>> Stashed changes
   await page.getByTestId("search-query").fill("Thor Hammer");
   await page.getByTestId("search-submit").click();
   await expect(productGrid.getByRole("link")).toHaveCount(1);
